9ad10c90fbc73a61718308db07d0f36566bdbf6d
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-06-22 14:38:41 -0500

    testeo de obtencion de datos de api Gutendex

--- src/main/java/com/andrmatt/challenge_literatura/principle/App.java
+++ src/main/java/com/andrmatt/challenge_literatura/principle/App.java
@@ -1,13 +1,21 @@
 package com.andrmatt.challenge_literatura.principle;
 
+import com.andrmatt.challenge_literatura.service.ConsumerAPI;
+
 public class App {
+
+    private final ConsumerAPI CONSUMER_API = new ConsumerAPI();
 
     public App() {
         this.runAppplication();
     }
 
     private void runAppplication() {
-       
+
+        System.out.println(
+                this.CONSUMER_API.getJsonData(
+                        "https://gutendex.com/books/"));
+
     }
 
 }
