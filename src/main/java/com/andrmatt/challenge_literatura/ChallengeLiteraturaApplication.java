a91baa137ce37331babd61ec08928cc84dfeef38
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-06-22 14:28:04 -0500

    cambio de propeidades env-template, creacion de clase principal

--- src/main/java/com/andrmatt/challenge_literatura/ChallengeLiteraturaApplication.java
+++ src/main/java/com/andrmatt/challenge_literatura/ChallengeLiteraturaApplication.java
@@ -1,13 +1,21 @@
 package com.andrmatt.challenge_literatura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andrmatt.challenge_literatura.principle.App;
+
 @SpringBootApplication
-public class ChallengeLiteraturaApplication {
+public class ChallengeLiteraturaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		new App();
 	}
 
 }
---
+++ src/main/java/com/andrmatt/challenge_literatura/principle/App.java
@@ -0,0 +1,13 @@
+package com.andrmatt.challenge_literatura.principle;
+
+public class App {
+
+    public App() {
+        this.runAppplication();
+    }
+
+    private void runAppplication() {
+       
+    }
+
+}
